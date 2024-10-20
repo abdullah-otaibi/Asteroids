f94af3c488251786e0cfc608bdb608f0ba1a7b05
abdullah-otaibi <ah.otaibi26@gmail.com>
2024-10-20 15:05:17 +0300

    added object grouping

--- main.py
+++ main.py
@@ -6,19 +6,33 @@ import pygame
 from constants import *
 from player import *
 
+
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     frames = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         dt = frames.tick(60) / 1000
 
